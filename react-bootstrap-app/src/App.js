eedb611972071273dafcebd68c073bfb9c987311
github username <github email address>
2018-10-12 10:15:04 -0400

    installed packages

--- react-bootstrap-app/src/App.js
+++ react-bootstrap-app/src/App.js
@@ -7,7 +7,7 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Oregon Trail</h1>
-        <User users={this.state.users} />
+        <User />
       </div>
     );
   }
