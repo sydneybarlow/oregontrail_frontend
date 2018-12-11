7d0b1fbcbbff612802fbe811ce8252d39ec20a12
github username <github email address>
2018-12-11 18:55:29 -0500

    Added snapshots in test

--- src/components/Homepage.test.js
+++ src/components/Homepage.test.js
@@ -6,18 +6,6 @@ import TestRenderer from "react-test-renderer";
 
 test("Game Start", () => {
   const component = TestRenderer.create(<Button>Get Goin!</Button>);
-  // let tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
-  //
-  // // manually trigger the callback
-  // tree.props.onMouseEnter();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
-  //
-  // // manually trigger the callback
-  // tree.props.onMouseLeave();
-  // // re-rendering
-  // tree = component.toJSON();
-  // expect(tree).toMatchSnapshot();
+  let example = component.toJSON();
+  expect(example).toMatchSnapshot();
 });
